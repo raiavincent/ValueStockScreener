848bc0254f0260fb692027c887973f3584e420f9
Vincent Raia <vincentraia1@gmail.com>
2021-03-31 23:46:32 -0400

    Update stockAttribPuller.py

--- stockAttribPuller.py
+++ stockAttribPuller.py
@@ -6,17 +6,7 @@ def pullStocks(ticker):
     try:
         stock = yf.Ticker(ticker)
         stockInfo = stock.info
-        # pbRatio = stockInfo.get('priceToBook')
-        # trailingPE = stockInfo.get('trailingPE')
-        # sector = stockInfo.get('sector')
-        # industry = stockInfo.get('industry')
-        # dividend = stockInfo.get('dividendRate')
-        # stockObj = stockquotes.Stock(ticker)
-        # currentPrice = stockObj.current_price
-        # stockInfo['Price'] = currentPrice
-        # print(ticker)
         stock_df = stock_df.append(stockInfo,ignore_index=True)
-        # stock_df = stock_df.append(priceDict,ignore_index=True)
     except Exception:
         # need to add Exception to except statement to allow for 
         # keyboard interrupt
