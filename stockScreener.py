94b7ac77e2fe16151c7022827001cb7446ec9c85
Vincent Raia <vincentraia1@gmail.com>
2021-04-02 11:57:13 -0400

    applymap str for stockscreener, was causing problems with gspread

--- stockScreener.py
+++ stockScreener.py
@@ -38,9 +38,9 @@ for ticker in tickers:
     except Exception:
         pass
     
-stock_df = stock_df[cols]
 stock_df = stock_df.dropna(axis=1,how='all')
-stock_df.fillna('N/A',inplace=True)
+stock_df.fillna('',inplace=True)
+stock_df = stock_df.applymap(str)
 
 tickerCount = len(stock_df.index)
 tickerCount = '{:,}'.format(tickerCount)
